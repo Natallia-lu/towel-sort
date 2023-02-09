39c656f511b5fe1d96f5a866071f14e3010a8ff1
Natallia-Lu <kali.laska@mail.ru>
2023-02-09 17:26:16 +0100

    feat towelsort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,39 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined) {
+    return ([])
+
+  }
+  let arr = [...matrix]
+  let n = arr.length;
+    let i = 0;
+    let result = [];
+     if(n!==0) {
+    while (i < n) {
+      if (i%2===1){
+         let add = arr[i].reverse();
+       result = result + add + ','
+
+      } else {
+           let elseadd = arr[i]
+  result = result + elseadd + ','
+
+      }
+      i = i + 1;
+    }
+      let newarr = result.split(',')
+      newarr.pop()
+
+let resres = newarr.map(string => +string)
+return resres
+    } else {
+    
+      return ([]) }
+
+      
+    
+
+
 }
+
